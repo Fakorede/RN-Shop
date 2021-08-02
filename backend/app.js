6c4b7e0ac7456c08ef823e3e11005abacf5fd6b9
fakorede <abiolafakorede@gmail.com>
2021-08-02 21:17:51 +0100

    server entry

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
 const productRoutes = require('./routes/productRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+const authJwt = require('./middlewares/authMiddleware')
+const errorHandler = require('./middlewares/errorMiddleware')
 
 dotenv.config()
 connectDB()
@@ -16,11 +20,16 @@ app.use(cors())
 app.options('*', cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(authJwt())
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
+app.use(errorHandler)
 
 const api = process.env.API_URL
 
+app.use(`${api}/users`, userRoutes)
 app.use(`${api}/categories`, categoryRoutes)
 app.use(`${api}/products`, productRoutes)
+app.use(`${api}/orders`, orderRoutes)
 
 const PORT = process.env.PORT || 4500
 
